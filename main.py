7407086658da20c290fc2f63a53b9d909f29c7e7
nico <nico@nico>
2026-03-22 02:02:46 +0300

    change

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ app.mount("/swagger", StaticFiles(directory="swagger_static"), name="swagger_fil
 # ----------------------------
 # CORS Middleware
 # ----------------------------
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 # ----------------------------
 # Routers
